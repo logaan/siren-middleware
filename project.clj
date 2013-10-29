143f87a6901ce611bb82c71109641ce61d4a7017
Logan Campbell <colin@logaan.net>
2013-10-29 22:26:33 +1100

    description

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
 (defproject siren-middleware "0.1.0-SNAPSHOT"
-  :description "FIXME"
+  :description "Ring middleware that enables content negotiation for siren with
+               json, smile, html, etc"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"} 
   :dependencies [[org.clojure/clojure "1.5.1"]
